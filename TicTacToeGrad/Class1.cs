cca8f98b34e42b1d700e59ae2f9fac321ae0e749
Pontus Wahlgren <pontus.wahlgren@winlas.se>
2024-12-18 08:43:21 +0100

    green test for board creation

--- TicTacToeGrad/Class1.cs
+++ TicTacToeGrad/Class1.cs
@@ -4,6 +4,15 @@ public class Board
 {
     public static char[,] CreateBoard()
     {
-        throw new NotImplementedException();
+        var board = new char[3, 3];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                board[i, j] = ' ';
+            }
+        }
+        
+        return board;
     }
 }
